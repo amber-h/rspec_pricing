de94138ab9184b597bcd49d4d4fb1bcdba45ace2
amber-h <amber.houle3@gmail.com>
2013-10-24 01:26:25 -0400

    adding specs to test text file parsing

--- spec/txtFileInput_spec.rb
+++ spec/txtFileInput_spec.rb
@@ -3,15 +3,42 @@ require 'txtFileInput'
 describe 'TxtFileInput' do
 	before :each do
 		@txtFile = "$1299.99 \n 3 people \n food"
+		@fileLines = ['$1299.99','3','food']
 	end
 
 	it 'should parse a text file into an array of strings' do
 		expect(@txtFile.split(/\n/)).to be_a(Array)
 	end
 
-	it 'should initially set the fileOuput to an empty hash' do
-		# fileOutput =
-		# expect(@txtFile.split(/\n/)).to be_a(Array)
+	describe 'converting txt file to type hash' do
+		it 'should return a hash containing product attributes' do
+			fileOutput = TxtFileInput.txtFileToHash(@fileLines)
+			expect(fileOutput).to be_a(Hash)
+		end
+		it 'should have a key basePrice' do
+			fileOutput = TxtFileInput.txtFileToHash(@fileLines)
+			fileOutput.should have_key(:basePrice)
+		end
+		it 'should have a key basePrice with value type Float' do
+			fileOutput = TxtFileInput.txtFileToHash(@fileLines)
+			expect(fileOutput[:basePrice]).to be_a(Float)
+		end
+		it 'should have a key numPeople' do
+			fileOutput = TxtFileInput.txtFileToHash(@fileLines)
+			fileOutput.should have_key(:numPeople)
+		end
+		it 'should have a key numPeople with value type Integer' do
+			fileOutput = TxtFileInput.txtFileToHash(@fileLines)
+			expect(fileOutput[:numPeople]).to be_a(Integer)
+		end
+		it 'should have a key type' do
+			fileOutput = TxtFileInput.txtFileToHash(@fileLines)
+			fileOutput.should have_key(:type)
+		end
+		it 'should have a key type with value type String' do
+			fileOutput = TxtFileInput.txtFileToHash(@fileLines)
+			expect(fileOutput[:type]).to be_a(String)
+		end
 	end
 
 end
