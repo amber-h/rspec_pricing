e5771a30df7a21c3bc1bb7d0e964617fa274a7c2
amber-h <amber.houle3@gmail.com>
2013-10-24 01:26:42 -0400

    refactoring txtFileInput to smaller methods

--- lib/txtFileInput.rb
+++ lib/txtFileInput.rb
@@ -1,14 +1,20 @@
 module TxtFileInput
 
 	def self.parseTxtFile(txtFile)
-	 	fileLines = File.open(txtFile, 'r').read.split(/\n/)
+	 	fileLines = readTxtFile(txtFile)
+	 	fileOutput = txtFileToHash(fileLines)
+	end
 
+	def self.readTxtFile(txtFile)
+		fileLines = File.open(txtFile, 'r').read.split(/\n/)
+	end
+
+	def self.txtFileToHash(fileLines)
 		fileOutput = {}
 	 	fileOutput[:basePrice] = fileLines[0].strip.sub('$','').to_f
 	 	fileOutput[:numPeople] = fileLines[1].strip.to_i
 	 	fileOutput[:type] = fileLines[2].strip
-
-	 	return fileOutput
+	 	fileOutput
 	end
 
 end
