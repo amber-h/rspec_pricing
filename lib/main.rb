39d1b47d9eef22eeb71541efb95e9b88df8b0bce
amber-h <amber.houle3@gmail.com>
2013-10-23 20:09:39 -0400

    calling methods to generate proper output in main

--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,13 @@
+require_relative 'product.rb'
+require_relative 'txtFileInput.rb'
+require_relative 'markupCalculator.rb'
+
+
 class Main
+
+	file = TxtFileInput.parseTxtFile('inputFiles/input1.txt')
+	product = Product.new(file[:basePrice], file[:numPeople], file[:type])
+	output = '$' + MarkupCalculator.markupCalc(product).to_s
+	puts output
 
 end
